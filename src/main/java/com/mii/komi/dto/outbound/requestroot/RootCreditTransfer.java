3baede8da24c6f9dee12bddeae326b1e405e9b83
adam <widjaja.adam@gmail.com>
2021-10-28 03:38:35 +0700

    Updated outbound request root, fixed ProxyResolutionResponse. Added ProxyResolutionParticipant --- might be incomplete/incorrect

--- src/main/java/com/mii/komi/dto/outbound/requestroot/RootCreditTransfer.java
+++ src/main/java/com/mii/komi/dto/outbound/requestroot/RootCreditTransfer.java
@@ -1,9 +1,23 @@
 package com.mii.komi.dto.outbound.requestroot;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mii.komi.dto.outbound.CreditTransferOutboundRequest;
+
 /**
  *
  * @author Erwin Sugianto Santoso - MII
  */
-public class RootCreditTransfer {
+public class RootCreditTransfer extends BaseRootHttpRequest {
     
+    @JsonProperty("CreditTransferRequest")
+    private CreditTransferOutboundRequest creditTransferOutboundRequest;
+
+    public CreditTransferOutboundRequest getCreditTransferOutboundRequest() {
+        return creditTransferOutboundRequest;
+    }
+
+    public void setCreditTransferOutboundRequest(CreditTransferOutboundRequest creditTransferOutboundRequest) {
+        this.creditTransferOutboundRequest = creditTransferOutboundRequest;
+    }
+
 }
--- src/main/java/com/mii/komi/dto/outbound/requestroot/RootProxyRegistration.java
+++ src/main/java/com/mii/komi/dto/outbound/requestroot/RootProxyRegistration.java
@@ -1,9 +1,23 @@
 package com.mii.komi.dto.outbound.requestroot;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mii.komi.dto.outbound.ProxyRegistrationRequest;
+
 /**
  *
  * @author Erwin Sugianto Santoso - MII
  */
-public class RootProxyRegistration {
+public class RootProxyRegistration extends BaseRootHttpRequest {
     
+    @JsonProperty("ProxyRegistrationRequest")
+    private ProxyRegistrationRequest proxyRegistrationRequest;
+
+    public ProxyRegistrationRequest getProxyRegistrationRequest() {
+        return proxyRegistrationRequest;
+    }
+
+    public void setProxyRegistrationRequest(ProxyRegistrationRequest proxyRegistrationRequest) {
+        this.proxyRegistrationRequest = proxyRegistrationRequest;
+    }
+
 }
