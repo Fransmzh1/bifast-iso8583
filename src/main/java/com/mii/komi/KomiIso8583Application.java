d6f24762d6ea5133db4c209cb08c8ab808f05bb5
Erwin Santoso <Erwin.Santoso@mii.co.id>
2021-11-01 17:42:22 +0700

    Remove unnecessary import on main class

--- src/main/java/com/mii/komi/KomiIso8583Application.java
+++ src/main/java/com/mii/komi/KomiIso8583Application.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
-import org.springframework.web.client.RestOperations;
 
 /**
  *
