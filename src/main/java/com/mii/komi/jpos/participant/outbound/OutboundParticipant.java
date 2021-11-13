f441c425bfd7c01169f59c3837736a4a4c6ae647
Erwin Santoso <Erwin.Santoso@mii.co.id>
2021-11-13 16:36:43 +0700

    remove unset (field 48) on response to core banking bank mantap

--- src/main/java/com/mii/komi/jpos/participant/outbound/OutboundParticipant.java
+++ src/main/java/com/mii/komi/jpos/participant/outbound/OutboundParticipant.java
@@ -71,7 +71,6 @@ public abstract class OutboundParticipant implements TransactionParticipant, Bas
             }
             try {
                 isoRsp.setResponseMTI();
-                isoRsp.unset(48);
                 return isoRsp;
             } catch (ISOException ex) {
                 Logger.getLogger(OutboundParticipant.class.getName()).log(Level.SEVERE, null, ex);
