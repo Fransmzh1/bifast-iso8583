76a7f5e22bae2b953ecae18d5f9e0f335a341c97
abdul fajar <abdul.fajar@mii.co.id>
2021-11-15 15:02:51 +0700

    fixing restResponse (responseCode & reasonCode)

--- src/main/java/com/mii/komi/controller/KomiDummyController.java
+++ src/main/java/com/mii/komi/controller/KomiDummyController.java
@@ -1,6 +1,7 @@
 package com.mii.komi.controller;
 
 import com.mii.komi.dto.outbound.AccountEnquiryOutboundRequest;
+import com.mii.komi.dto.outbound.AccountEnquiryOutboundResponse;
 import com.mii.komi.dto.outbound.AccountEnquiryOutboundResponseDummy;
 import com.mii.komi.dto.outbound.RestResponse;
 import io.swagger.annotations.Api;
@@ -10,15 +11,22 @@ import io.swagger.annotations.ApiResponses;
 import org.jpos.iso.ISOException;
 import org.jpos.util.NameRegistrar;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "", allowedHeaders = "", methods = {
+        RequestMethod.GET,
+        RequestMethod.POST,
+        RequestMethod.OPTIONS,
+        RequestMethod.PUT,
+        RequestMethod.PATCH,
+        RequestMethod.HEAD,
+        RequestMethod.DELETE,
+        RequestMethod.TRACE})
 @RestController
 @RequestMapping("/komidummy/api")
 @Api(tags = {"ISO8583 Adapter API"})
@@ -31,13 +39,13 @@ public class KomiDummyController {
             @ApiResponse(code = 403, message = "Forbidden"),
             @ApiResponse(code = 404, message = "Page Not Found")
     })
-    @PostMapping(path = "/accountinquiry", produces = MediaType.APPLICATION_JSON_VALUE)
-    public RestResponse<RestResponse> accountEnquiry(
+    @RequestMapping(path = "/accountinquiry", produces = MediaType.APPLICATION_JSON_VALUE,  method = RequestMethod.POST)
+    public ResponseEntity<RestResponse> accountEnquiry(
             @RequestBody AccountEnquiryOutboundRequest request,
             HttpServletRequest httpServletRequest) throws ISOException, NameRegistrar.NotFoundException {
 //        ResponseEntity rsp = queryTxnMgr(request, "AccountEnquiry");
-        List<AccountEnquiryOutboundResponseDummy> list = new ArrayList<>();
-        AccountEnquiryOutboundResponseDummy rspDummy = new AccountEnquiryOutboundResponseDummy();
+        List<AccountEnquiryOutboundResponse> list = new ArrayList<>();
+        AccountEnquiryOutboundResponse rspDummy = new AccountEnquiryOutboundResponse();
         rspDummy.setAccountNumber("677589");
         rspDummy.setAccountType("CACC");
         rspDummy.setCreditorId("KTP-208472701");
@@ -46,7 +54,7 @@ public class KomiDummyController {
         rspDummy.setCreditorType("01");
         rspDummy.setResidentStatus("01");
         rspDummy.setTownName("0300");
-        rspDummy.setNoRef("000000556773");
+        rspDummy.setNoRef("20200420471644500811");
 
         list.add(rspDummy);
         String rsp ="{\n" +
@@ -55,7 +63,8 @@ public class KomiDummyController {
                 "   \"ReasonMessage\": \"Success/ Transaction Accepted\",\n" +
                 "   \"Date\": \"20211002\",\n" +
                 "   \"Time\": \"153500\",\n" +
-                "   \"Content\": [{\n" +
+                "   \"Content\": [" +
+                "{\n" +
                 "\"NoRef\": \"000000556773\",\n" +
                 "\"AccountNumber\": \"677589\",\n" +
                 "\"AccountType\": \"CACC\",\n" +
@@ -64,7 +73,9 @@ public class KomiDummyController {
                 "\"CreditorType\": \"01\",\n" +
                 "\"ResidentStatus\": \"01\",\n" +
                 "\"TownName\": \"0300\"\n" +
-                "}] }";
+                "}" +
+                "]" +
+                " }";
 
         String rjct ="{\n" +
                 "   \"ResponseCode\": \"RJCT\",\n" +
@@ -87,8 +98,9 @@ public class KomiDummyController {
                 "\"NoRef\": \"202010204556773\",\n" +
                 "\"AccountNumber\": \"677589\"\n" +
                 "}] }";
-
-        return RestResponse.success("Success/ Transaction Accepted",list);
+    String test = String.valueOf(RestResponse.success("Success/ Transaction Accepted",list));
+        System.out.println("fajar " + rsp + "  " +rsp.length());
+        return ResponseEntity.ok(RestResponse.success("Success/ Transaction Accepted",list));
     }
 
 }
